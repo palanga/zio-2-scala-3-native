3f26a5ba42768820633dafd7bb0100de970bb83d
Andrés González <a.gonzalez.terres@gmail.com>
2022-09-16 12:02:49 -0300

    wip format

--- src/main/scala/socket/Address.scala
+++ src/main/scala/socket/Address.scala
@@ -20,13 +20,28 @@ class Address private (underlying: sockaddr_in):
   private[socket] def asSocketAddressPointer: Ptr[sockaddr] = underlying.toPtr.asInstanceOf[Ptr[sockaddr]]
 
 /**
- * #include <stdio.h> #include <stdlib.h> #include <sys/socket.h> #include <netinet/in.h> #include <netdb.h>
+ * {{{
+ * #include <stdio.h>
+ * #include <stdlib.h>
+ * #include <sys/socket.h>
+ * #include <netinet/in.h>
+ * #include <netdb.h>
  *
- * void init_sockaddr (struct sockaddr_in *name, const char *hostname, uint16_t port) { struct hostent *hostinfo;
+ * void init_sockaddr (struct sockaddr_in *name, const char *hostname, uint16_t port) {
+ *   struct hostent *hostinfo;
  *
- * name->sin_family = AF_INET; name->sin_port = htons (port); hostinfo = gethostbyname (hostname); if (hostinfo == NULL)
- * { fprintf (stderr, "Unknown host %s.\n", hostname); exit (EXIT_FAILURE); } name->sin_addr = *(struct in_addr *)
- * hostinfo->h_addr; }
+ *   name->sin_family = AF_INET;
+ *   name->sin_port = htons (port);
+ *
+ *   hostinfo = gethostbyname (hostname);
+ *   if (hostinfo == NULL) {
+ *     fprintf (stderr, "Unknown host %s.\n", hostname);
+ *     exit (EXIT_FAILURE);
+ *   }
+ *
+ *   name->sin_addr = *(struct in_addr *) hostinfo->h_addr;
+ * }
+ * }}}
  */
 object Address:
 
