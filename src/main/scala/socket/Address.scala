19b6e0b05002d405dfc7c65978c3cb2ab335eb6b
Andrés González <a.gonzalez.terres@gmail.com>
2022-09-18 09:39:46 -0300

    wip address info and address name

--- src/main/scala/socket/Address.scala
+++ src/main/scala/socket/Address.scala
@@ -6,12 +6,13 @@ import scala.scalanative
 import scala.scalanative.posix
 import scala.scalanative.libc
 import scala.scalanative.libc.stdlib
-import scala.scalanative.libc.string.{ strerror, strlen }
+import scala.scalanative.libc.string.{strerror, strlen}
 import scala.scalanative.posix.inttypes.uint16_t
-import scala.scalanative.posix.netinet.in.{ in_addr, sockaddr_in }
+import scala.scalanative.posix.netdb.addrinfo
+import scala.scalanative.posix.netinet.in.{in_addr, sockaddr_in}
 import scala.scalanative.posix.sys.socket
-import scala.scalanative.posix.sys.socket.{ sockaddr, socklen_t }
-import scala.scalanative.unsafe.{ sizeof, stackalloc, toCString, CChar, CInt, CString, Ptr, Zone }
+import scala.scalanative.posix.sys.socket.{sockaddr, socklen_t}
+import scala.scalanative.unsafe.{CChar, CInt, CString, Ptr, Zone, fromCString, sizeof, stackalloc, toCString}
 import scala.scalanative.unsigned.UInt
 
 class Address private (underlying: sockaddr_in):
@@ -39,26 +40,200 @@ class Address private (underlying: sockaddr_in):
 
 /**
  * {{{
- * #include <stdio.h>
- * #include <stdlib.h>
- * #include <sys/socket.h>
- * #include <netinet/in.h>
- * #include <netdb.h>
  *
- * void init_sockaddr (struct sockaddr_in *name, const char *hostname, uint16_t port) {
- *   struct hostent *hostinfo;
- *
- *   name->sin_family = AF_INET;
- *   name->sin_port = htons (port);
- *
- *   hostinfo = gethostbyname (hostname);
- *   if (hostinfo == NULL) {
- *     fprintf (stderr, "Unknown host %s.\n", hostname);
- *     exit (EXIT_FAILURE);
- *   }
- *
- *   name->sin_addr = *(struct in_addr *) hostinfo->h_addr;
- * }
+       The following programs demonstrate the use of getaddrinfo(),
+       gai_strerror(), freeaddrinfo(), and getnameinfo(3).  The programs
+       are an echo server and client for UDP datagrams.
+
+   Server program
+
+       #include <sys/types.h>
+       #include <stdio.h>
+       #include <stdlib.h>
+       #include <unistd.h>
+       #include <string.h>
+       #include <sys/socket.h>
+       #include <netdb.h>
+
+       #define BUF_SIZE 500
+
+       int
+       main(int argc, char *argv[])
+       {
+           struct addrinfo hints;
+           struct addrinfo *result, *rp;
+           int sfd, s;
+           struct sockaddr_storage peer_addr;
+           socklen_t peer_addr_len;
+           ssize_t nread;
+           char buf[BUF_SIZE];
+
+           if (argc != 2) {
+               fprintf(stderr, "Usage: %s port\n", argv[0]);
+               exit(EXIT_FAILURE);
+           }
+
+           memset(&hints, 0, sizeof(hints));
+           hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+           hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+           hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
+           hints.ai_protocol = 0;          /* Any protocol */
+           hints.ai_canonname = NULL;
+           hints.ai_addr = NULL;
+           hints.ai_next = NULL;
+
+           s = getaddrinfo(NULL, argv[1], &hints, &result);
+           if (s != 0) {
+               fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+               exit(EXIT_FAILURE);
+           }
+
+           /* getaddrinfo() returns a list of address structures.
+              Try each address until we successfully bind(2).
+              If socket(2) (or bind(2)) fails, we (close the socket
+              and) try the next address. */
+
+           for (rp = result; rp != NULL; rp = rp->ai_next) {
+               sfd = socket(rp->ai_family, rp->ai_socktype,
+                       rp->ai_protocol);
+               if (sfd == -1)
+                   continue;
+
+               if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0)
+                   break;                  /* Success */
+
+               close(sfd);
+           }
+
+           freeaddrinfo(result);           /* No longer needed */
+
+           if (rp == NULL) {               /* No address succeeded */
+               fprintf(stderr, "Could not bind\n");
+               exit(EXIT_FAILURE);
+           }
+
+           /* Read datagrams and echo them back to sender. */
+
+           for (;;) {
+               peer_addr_len = sizeof(peer_addr);
+               nread = recvfrom(sfd, buf, BUF_SIZE, 0,
+                       (struct sockaddr *) &peer_addr, &peer_addr_len);
+               if (nread == -1)
+                   continue;               /* Ignore failed request */
+
+               char host[NI_MAXHOST], service[NI_MAXSERV];
+
+               s = getnameinfo((struct sockaddr *) &peer_addr,
+                               peer_addr_len, host, NI_MAXHOST,
+                               service, NI_MAXSERV, NI_NUMERICSERV);
+               if (s == 0)
+                   printf("Received %zd bytes from %s:%s\n",
+                           nread, host, service);
+               else
+                   fprintf(stderr, "getnameinfo: %s\n", gai_strerror(s));
+
+               if (sendto(sfd, buf, nread, 0,
+                           (struct sockaddr *) &peer_addr,
+                           peer_addr_len) != nread)
+                   fprintf(stderr, "Error sending response\n");
+           }
+       }
+
+   Client program
+
+       #include <sys/types.h>
+       #include <sys/socket.h>
+       #include <netdb.h>
+       #include <stdio.h>
+       #include <stdlib.h>
+       #include <unistd.h>
+       #include <string.h>
+
+       #define BUF_SIZE 500
+
+       int
+       main(int argc, char *argv[])
+       {
+           struct addrinfo hints;
+           struct addrinfo *result, *rp;
+           int sfd, s;
+           size_t len;
+           ssize_t nread;
+           char buf[BUF_SIZE];
+
+           if (argc < 3) {
+               fprintf(stderr, "Usage: %s host port msg...\n", argv[0]);
+               exit(EXIT_FAILURE);
+           }
+
+           /* Obtain address(es) matching host/port. */
+
+           memset(&hints, 0, sizeof(hints));
+           hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+           hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+           hints.ai_flags = 0;
+           hints.ai_protocol = 0;          /* Any protocol */
+
+           s = getaddrinfo(argv[1], argv[2], &hints, &result);
+           if (s != 0) {
+               fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+               exit(EXIT_FAILURE);
+           }
+
+           /* getaddrinfo() returns a list of address structures.
+              Try each address until we successfully connect(2).
+              If socket(2) (or connect(2)) fails, we (close the socket
+              and) try the next address. */
+
+           for (rp = result; rp != NULL; rp = rp->ai_next) {
+               sfd = socket(rp->ai_family, rp->ai_socktype,
+                            rp->ai_protocol);
+               if (sfd == -1)
+                   continue;
+
+               if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
+                   break;                  /* Success */
+
+               close(sfd);
+           }
+
+           freeaddrinfo(result);           /* No longer needed */
+
+           if (rp == NULL) {               /* No address succeeded */
+               fprintf(stderr, "Could not connect\n");
+               exit(EXIT_FAILURE);
+           }
+
+           /* Send remaining command-line arguments as separate
+              datagrams, and read responses from server. */
+
+           for (int j = 3; j < argc; j++) {
+               len = strlen(argv[j]) + 1;
+                       /* +1 for terminating null byte */
+
+               if (len > BUF_SIZE) {
+                   fprintf(stderr,
+                           "Ignoring long message in argument %d\n", j);
+                   continue;
+               }
+
+               if (write(sfd, argv[j], len) != len) {
+                   fprintf(stderr, "partial/failed write\n");
+                   exit(EXIT_FAILURE);
+               }
+
+               nread = read(sfd, buf, BUF_SIZE);
+               if (nread == -1) {
+                   perror("read");
+                   exit(EXIT_FAILURE);
+               }
+
+               printf("Received %zd bytes: %s\n", nread, buf);
+           }
+
+           exit(EXIT_SUCCESS);
+       }
+SEE ALSO         top
  * }}}
  */
 object Address:
@@ -93,19 +268,40 @@ object Address:
     Zone { implicit z =>
       import scala.scalanative.posix.arpa.inet.{htons, inet_pton}
       import scalanative.unsigned.UnsignedRichInt
+      import scalanative.posix.netdbOps.*
 
       val cHost = toCString(host)
-      val inAddr: Ptr[in_addr] = stackalloc[in_addr]()
-      val exitCode = inet_pton(
-        posix.sys.socket.AF_INET,
-        cHost,
-        inAddr.asInstanceOf[Ptr[Byte]],
-      )
-      if exitCode != 1 then throw Exception("invalid host or port") else ()
+//      val inAddr: Ptr[in_addr] = stackalloc[in_addr]()
+//      val exitCode = inet_pton(
+//        posix.sys.socket.AF_INET,
+//        cHost,
+//        inAddr.asInstanceOf[Ptr[Byte]],
+//      )
+//      if exitCode != 1 then throw Exception("invalid host or port") else ()
 
-      Address(cSocketAddress(inAddr, htons(port.toUShort)))
+
+      val reses: Ptr[Ptr[addrinfo]] = stackalloc[Ptr[addrinfo]]()
+      posix.netdb.getaddrinfo(cHost, toCString(port.toString), null, reses) // TODO don't forget to free address info
+
+      val res: Ptr[addrinfo] = reses(0)
+
+      val printHost = toCString("                ")
+      val pritnPort = toCString("                ") // TODO me transforma el numero del puerto a un nombre en particular (8080 es http-alt)
+      posix.netdb.getnameinfo(res.ai_addr, sizeof[sockaddr_in].toUInt, printHost, 16.toUShort, pritnPort, 16.toUShort, 0)
+
+      println(s"la direccion al final es: ${fromCString(printHost)}:${fromCString(pritnPort)}")
+
+
+      import scala.scalanative.posix.netinet.inOps.*
+      res.ai_family = posix.sys.socket.AF_INET
+      Address(cSocketAddress(res.ai_addr.asInstanceOf[Ptr[sockaddr_in]].sin_addr, htons(port.toUShort))) // TODO me dice address family not supported
+//      Address(cSocketAddress(inAddr, htons(port.toUShort)))
     }
   }.debug
+
+  def getAddressInfo(host: String, port: Int): Task[(Int, Int)] = ???
+
+  def getAddressName(host: Int, port: Int): (String, Int) = ???
 
 
   // TODO not tested and should be in common package
